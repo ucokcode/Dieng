c0e8fea738e7387e28ebd7d1def09c17b7c4c5fa
ucokcode <ucokcode00@gmail.com>
2020-09-11 00:25:10 +0700

    .
    Signed-off-by: ucokcode <ucokcode00@gmail.com>

--- app/src/main/java/dev/ucokcode/dieng/ui/Jelajah.kt
+++ app/src/main/java/dev/ucokcode/dieng/ui/Jelajah.kt
@@ -1,3 +1,11 @@
+/*
+ * *
+ *  * Created by Harapan Pardamean on 9/11/20 12:25 AM
+ *  * Copyright (c) 2020 . All rights reserved.
+ *  * Last modified 9/11/20 12:24 AM
+ *
+ */
+
 package dev.ucokcode.dieng.ui
 
 import android.os.Bundle
