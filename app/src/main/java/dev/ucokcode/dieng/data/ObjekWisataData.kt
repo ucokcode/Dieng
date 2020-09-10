8889d72d19f4ca6b04bfb3a5ff119784f701b4f6
ucokcode <ucokcode00@gmail.com>
2020-09-10 21:25:29 +0700

    .

--- app/src/main/java/dev/ucokcode/dieng/data/ObjekWisataData.kt
+++ app/src/main/java/dev/ucokcode/dieng/data/ObjekWisataData.kt
@@ -4,19 +4,10 @@ import dev.ucokcode.dieng.R
 import dev.ucokcode.dieng.model.Wisata
 
 object ObjekWisataData {
-    val listObjek: ArrayList<Wisata> =
+    val list: ArrayList<Wisata> =
         arrayListOf<Wisata>(
-            Wisata(
-                1,
-                "Kawah candradimuk",
-                R.drawable.o_kawahcadradimuka,
-                "blablablabla"
-            ), Wisata(
-                2,
-                "Telaga Warna",
-                R.drawable.o_telagawarna,
-                "blablabla"
-            )
+            Wisata(1, "Kawah candradimuk", R.drawable.o_kawahcadradimuka, "blablablabla"),
+            Wisata(2, "Telaga Warna", R.drawable.o_telagawarna, "blablabla")
         )
 }
 
